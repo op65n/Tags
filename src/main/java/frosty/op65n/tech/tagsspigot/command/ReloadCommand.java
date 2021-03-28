3196b03dd8666a6e333db873b7e95d835fa96e12
Frcsty <frostyprime13@gmail.com>
2021-03-28 12:06:18 +0200

    added reconnect command

--- src/main/java/frosty/op65n/tech/tagsspigot/command/ReloadCommand.java
+++ src/main/java/frosty/op65n/tech/tagsspigot/command/ReloadCommand.java
@@ -1,13 +1,13 @@
 package frosty.op65n.tech.tagsspigot.command;
 
 import frosty.op65n.tech.tagsspigot.TagsPlugin;
+import frosty.op65n.tech.tagsspigot.database.Database;
 import frosty.op65n.tech.tagsspigot.util.TaskUtil;
 import me.mattstudios.mf.annotations.Command;
 import me.mattstudios.mf.annotations.Permission;
 import me.mattstudios.mf.annotations.SubCommand;
 import me.mattstudios.mf.base.CommandBase;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 @Command("tags")
 public final class ReloadCommand extends CommandBase {
@@ -25,6 +25,18 @@ public final class ReloadCommand extends CommandBase {
             plugin.getRegistry().request();
 
             sender.sendMessage("Gawk Gawk Requested updated config from Database.");
+        });
+    }
+
+    @SubCommand("reconnect")
+    @Permission("tags.command.reconnect")
+    public void onReconnect(final CommandSender sender) {
+        TaskUtil.async(() -> {
+            Database.INSTANCE.terminateAdapter();
+
+            Database.INSTANCE.createAdapter();
+
+            sender.sendMessage("Gawk Gawk Successfully reconnected the Database Adapter.");
         });
     }
 
